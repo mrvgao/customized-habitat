81d619e34b6b5cd7468919891282efa51310c379
mrvgao <minchiuan@zju.edu.cn>
2024-10-20 20:38:47 -0400

    add strecth

--- examples/interactive_play.py
+++ examples/interactive_play.py
@@ -369,7 +369,8 @@ def get_input_vel_ctlr(
     print('base_action is not None', base_action is not None)
     print('base_action_name in env.action_space.spaces', base_action_name in env.action_space.spaces)
 
-    if base_action is not None and base_action_name in env.action_space.spaces:
+    # if base_action is not None and base_action_name in env.action_space.spaces:
+    if base_action is not None:
         name = base_action_name
         args = {base_key: base_action}
     else:
